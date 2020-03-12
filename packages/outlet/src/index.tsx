87245e3405fc86bf85b89c10b230c4323615f81c
Steve Sims <steve_sims7@yahoo.co.uk>
2020-03-12 17:27:07 +0000

    fix: slightly more explicit outlet component type

--- packages/outlet/src/index.tsx
+++ packages/outlet/src/index.tsx
@@ -11,7 +11,7 @@ const Outlet: React.FC<OutletProps> = ({
   ...props
 }: OutletProps) => (
   <>
-    {get(registryKey).map((Component, index) => (
+    {get(registryKey).map((Component: React.ComponentType, index) => (
       <Component key={index} {...props} />
     ))}
   </>
