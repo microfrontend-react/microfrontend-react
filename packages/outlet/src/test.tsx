efb47bfa0bbbc57145d464c9b19b45d3cf2937ef
Steve Sims <steve_sims7@yahoo.co.uk>
2020-03-13 11:50:21 +0000

    chore: add more/better tests
    also hopefully fix publishing build

--- packages/outlet/src/test.tsx
+++ packages/outlet/src/test.tsx
@@ -1,11 +1,54 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { register } from '@microfrontend-react/core';
+
 import Outlet from '.';
 
 describe('@microfrontend-react/outlet', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Outlet registryKey="test" />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<Outlet registryKey="outlet-test" />);
+  });
+
+  it('renders nothing when the key is not matched to anything in the registry', () => {
+    const { container } = render(
+      <Outlet registryKey="outlet-test-unknown-key">
+        Children should be ignored
+      </Outlet>
+    );
+
+    expect(container.innerHTML).toBeFalsy();
+  });
+
+  it('renders all registered components', () => {
+    const registryKey = 'outlet-render-registered';
+    const simpleText = 'Super simple component';
+    const passThruText = 'Children can be passed thru';
+
+    register(registryKey, () => <div data-testid="simple">{simpleText}</div>);
+    register(registryKey, (props: any) => (
+      <div data-testid="pass-thru" {...props} />
+    ));
+
+    const { getByText, getByTestId } = render(
+      <Outlet registryKey={registryKey}>{passThruText}</Outlet>
+    );
+
+    expect(getByText(simpleText)).toBeDefined();
+    expect(getByTestId('simple')).toBeDefined();
+
+    expect(getByText(passThruText)).toBeDefined();
+    expect(getByTestId('pass-thru').innerHTML).toEqual(passThruText);
+  });
+
+  it('can be nested', () => {
+    const innerRegistryKey = 'outlet-inner';
+    const outerRegistryKey = 'outlet-outer';
+
+    register(innerRegistryKey, () => <div data-testid="inner">Inner item</div>);
+    register(outerRegistryKey, () => <Outlet registryKey={innerRegistryKey} />);
+
+    const { getByTestId } = render(<Outlet registryKey={outerRegistryKey} />);
+
+    expect(getByTestId('inner')).toBeDefined();
   });
 });
